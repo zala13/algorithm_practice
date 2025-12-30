f9ce362910b5d7ee87ef857686c1c9a004054016
zala13 <124238203+zala13@users.noreply.github.com>
2025-12-31 00:54:00 +0800

    leetcode feat 121.买卖股票的最佳时机 1. 典型股票问题，有套路可以遵循
    参考：
    dp[i][k][0 or 1]
    0 <= i <= n - 1, 1 <= k <= K
    n 为天数，大 K 为交易数的上限，0 和 1 代表是否持有股票。
    
    dp[i][k][0] = max(dp[i-1][k][0],     dp[i-1][k][1] + prices[i])
                        max( 今天选择 rest,  今天选择 sell)
    
    dp[i][k][1] = max(dp[i-1][k][1],      dp[i-1][k-1][0] - prices[i])
                        max( 今天选择 rest,  今天选择 buy)

--- 121.买卖股票的最佳时机.java
+++ 121.买卖股票的最佳时机.java
@@ -8,7 +8,20 @@
 // @lc code=start
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.len
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        // dp[i][0 / 1] 代表第i天是否持有股票利润
+        // i < j,否则利润为0，所以边界情况都置0
+        for (int i = 0; i < n; i++) {
+            if (i - 1 == -1) {
+                dp[i][0] = 0;
+                dp[i][1] = -prices[i];
+                continue;
+            }
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(-prices[i], dp[i - 1][1]);
+        }
+        return dp[n - 1][0];
     }
 }
 // @lc code=end
