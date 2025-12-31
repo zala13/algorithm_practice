53b989df56a5bde4971f3479b439bccf9dfa73f8
zala13 <124238203+zala13@users.noreply.github.com>
2025-12-31 23:24:55 +0800

    leetcode feat 141.环形链表 - 快慢指针，秒了

---
+++ 141.环形链表.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=java
+ * @lcpr version=30305
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [3,2,0,-4]\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n0\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n-1\n
+// @lcpr case=end
+
+ */
+
