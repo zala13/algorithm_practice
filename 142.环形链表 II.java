89cb19917e65fbe53d228fe7495396ce053553fc
zala13 <124238203+zala13@users.noreply.github.com>
2025-12-31 23:24:32 +0800

    leetcode feat 142.环形链表 1. 重点在于要能够讲清楚为什么根据第一次的相遇点推断出链表交点

---
+++ 142.环形链表 II.java
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=java
+ * @lcpr version=30305
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [3,2,0,-4]\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n0\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n-1\n
+// @lcpr case=end
+
+ */
+
