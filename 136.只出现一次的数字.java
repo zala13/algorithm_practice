59bc74d5a3c7301d4c7783ff2419db13fd6a0f78
zala13 <zalainsz@outlook.com>
2026-01-02 23:46:30 +0800

    Leetcode feat [136] 只出现一次的数字 1. 一生之痛，异或

---
+++ 136.只出现一次的数字.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=136 lang=java
+ * @lcpr version=30305
+ *
+ * [136] 只出现一次的数字
+ */
+
+// @lc code=start
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res ^= nums[i];
+        }
+        return res;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [2,2,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [4,1,2,1,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+ */
+
