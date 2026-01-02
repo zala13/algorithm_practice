ba0fd906c6a91bf4baa2cbd48797b46cc3713b84
zala13 <zalainsz@outlook.com>
2026-01-02 23:52:05 +0800

    Leetcode feat [141] 环形链表 1. 经典题目无需多盐

--- 141.环形链表.java
+++ 141.环形链表.java
@@ -19,7 +19,15 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 // @lc code=end
