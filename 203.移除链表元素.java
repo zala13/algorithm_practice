7c1748c67a0c3d50320ad4b794dd8e416a8498cb
zala13 <zalainsz@outlook.com>
2026-01-07 23:27:07 +0800

    Leetcode feat [203] 移除链表元素 1. 基础题目

---
+++ 203.移除链表元素.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=java
+ * @lcpr version=30305
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode curr = dummy;
+        while (curr != null && curr.next != null) {
+            while (curr != null && curr.next != null && curr.next.val == val) {
+                curr.next = curr.next.next;
+            }
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,6,3,4,5,6]\n6\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [7,7,7,7]\n7\n
+// @lcpr case=end
+
+ */
+
