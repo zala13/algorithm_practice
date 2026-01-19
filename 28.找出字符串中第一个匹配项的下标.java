b94513f349c9908ebac587922db53ee19eef2805
zala13 <zalainsz@outlook.com>
2026-01-20 00:10:41 +0800

    Leetcode feat 字符串

--- 28.找出字符串中第一个匹配项的下标.java
+++ 28.找出字符串中第一个匹配项的下标.java
@@ -7,8 +7,18 @@
 
 // @lc code=start
 class Solution {
-    public int strStr(String haystack, String needle) {
-        
+    public int strStr(String txt, String pat) {
+        int N = txt.length(), L = pat.length();
+
+        for (int i = 0; i + L <= N; i++) {
+            String subStr = txt.substring(i, i + L);
+            if (subStr.equals(pat)){
+                // 在 txt 中找到模式串 pat，返回起始索引
+                return i;
+            }
+        }
+        // txt 中不存在模式串 pat
+        return -1;
     }
 }
 // @lc code=end
