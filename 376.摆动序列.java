956ff77190569d8f0b138b9611dfa7df2ba47d85
zala <zalainsz@outlook.com>
2026-03-02 22:08:59 +0800

    Leetcode feat 摆动序列

--- 376.摆动序列.java
+++ 376.摆动序列.java
@@ -8,7 +8,24 @@
 // @lc code=start
 class Solution {
     public int wiggleMaxLength(int[] nums) {
-        
+        int n = nums.length;
+        if (n < 2) return n;
+
+        int up = 1;    // 以nums[0]结尾，最后是上升的最长长度
+        int down = 1;  // 以nums[0]结尾，最后是下降的最长长度
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                // 当前是上升 -> up = down + 1（之前必须是下降状态）
+                up = down + 1;
+            } else if (nums[i] < nums[i - 1]) {
+                // 当前是下降 -> down = up + 1（之前必须是上升状态）
+                down = up + 1;
+            }
+            // 相等时，up和down保持不变
+        }
+
+        return Math.max(up, down);
     }
 }
 // @lc code=end
